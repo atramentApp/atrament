e08a53ee5b91590681b52986acb1be5603d683aa
Atrament <mo.gawin81@gmail.com>
2026-08-22 19:16:03 +0700

    Refactor Player class for improved mechanics
    Updated player dimensions, speed, jump force, and gravity values. Improved collision detection and drawing methods.

--- js/player.js
+++ js/player.js
@@ -1,98 +1,73 @@
-// Player character for Atrament
+// Improved Player for Atrament
 
 class Player {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.width = 26;
-    this.height = 38;
+    this.width = 28;
+    this.height = 40;
 
     this.velocityX = 0;
     this.velocityY = 0;
 
-    this.speed = 5.2;
-    this.jumpForce = -12.5;
-    this.gravity = 0.48;
-    this.maxFallSpeed = 14;
+    this.speed = 5.1;
+    this.jumpForce = -12.8;
+    this.gravity = 0.47;
+    this.maxFallSpeed = 13.5;
 
     this.onGround = false;
-    this.wasOnGround = false;
-
-    // Coyote time & jump buffer
     this.coyoteTime = 0;
-    this.coyoteMax = 8;
+    this.coyoteMax = 9;
     this.jumpBuffer = 0;
     this.jumpBufferMax = 8;
 
     this.facing = 1;
-    this.color = "#121212";
+    this.animTimer = 0;
   }
 
   update(platforms) {
-    this.wasOnGround = this.onGround;
+    this.animTimer++;
 
-    // Gravity
     this.velocityY += this.gravity;
-    if (this.velocityY > this.maxFallSpeed) {
-      this.velocityY = this.maxFallSpeed;
-    }
+    if (this.velocityY > this.maxFallSpeed) this.velocityY = this.maxFallSpeed;
 
-    // Apply movement
     this.x += this.velocityX;
     this.y += this.velocityY;
 
     this.onGround = false;
-
-    // Collision with platforms
     this.handleCollisions(platforms);
 
-    // Coyote time
     if (this.onGround) {
       this.coyoteTime = this.coyoteMax;
     } else {
       this.coyoteTime--;
     }
 
-    // Jump buffer
     if (this.jumpBuffer > 0) {
       this.jumpBuffer--;
-      if (this.coyoteTime > 0) {
-        this.doJump();
-      }
+      if (this.coyoteTime > 0) this.doJump();
     }
 
-    // Facing
     if (this.velocityX > 0.3) this.facing = 1;
     if (this.velocityX < -0.3) this.facing = -1;
 
-    // Keep inside screen
     if (this.x < 0) this.x = 0;
-    if (this.x + this.width > canvas.width) {
-      this.x = canvas.width - this.width;
-    }
+    if (this.x + this.width > canvas.width) this.x = canvas.width - this.width;
   }
 
   handleCollisions(platforms) {
     for (let p of platforms) {
       if (this.intersects(p)) {
-        // Land on top
-        if (this.velocityY > 0 && this.y + this.height - this.velocityY <= p.y + 10) {
+        if (this.velocityY > 0 && this.y + this.height - this.velocityY <= p.y + 12) {
           this.y = p.y - this.height;
           this.velocityY = 0;
           this.onGround = true;
-        }
-        // Hit ceiling
-        else if (this.velocityY < 0 && this.y - this.velocityY >= p.y + p.height - 10) {
+        } else if (this.velocityY < 0 && this.y - this.velocityY >= p.y + p.height - 12) {
           this.y = p.y + p.height;
           this.velocityY = 0;
-        }
-        // Side collision
-        else {
-          if (this.velocityX > 0) {
-            this.x = p.x - this.width;
-          } else if (this.velocityX < 0) {
-            this.x = p.x + p.width;
-          }
+        } else {
+          if (this.velocityX > 0) this.x = p.x - this.width;
+          else if (this.velocityX < 0) this.x = p.x + p.width;
           this.velocityX = 0;
         }
       }
@@ -110,9 +85,7 @@ class Player {
 
   jump() {
     this.jumpBuffer = this.jumpBufferMax;
-    if (this.coyoteTime > 0) {
-      this.doJump();
-    }
+    if (this.coyoteTime > 0) this.doJump();
   }
 
   doJump() {
@@ -123,20 +96,41 @@ class Player {
   }
 
   draw(ctx) {
-    // Body
-    ctx.fillStyle = this.color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    const cx = this.x + this.width / 2;
+    const bob = this.onGround ? Math.sin(this.animTimer * 0.12) * 1.2 : 0;
+
+    // Cloak / body
+    ctx.fillStyle = "#0f0f0f";
+    ctx.beginPath();
+    ctx.moveTo(this.x + 4, this.y + 14 + bob);
+    ctx.quadraticCurveTo(cx, this.y + 8 + bob, this.x + this.width - 4, this.y + 14 + bob);
+    ctx.lineTo(this.x + this.width - 2, this.y + this.height + bob);
+    ctx.lineTo(this.x + 2, this.y + this.height + bob);
+    ctx.closePath();
+    ctx.fill();
 
     // Hood
     ctx.beginPath();
-    ctx.arc(this.x + this.width / 2, this.y + 11, 13, Math.PI, 0);
+    ctx.arc(cx, this.y + 13 + bob, 14, Math.PI * 1.05, Math.PI * -0.05);
     ctx.fill();
 
-    // Eye
-    ctx.fillStyle = "#e8d5b7";
-    const eyeX = this.facing === 1 ? this.x + 17 : this.x + 7;
+    // Inner hood shadow
+    ctx.fillStyle = "#080808";
     ctx.beginPath();
-    ctx.arc(eyeX, this.y + 14, 2.2, 0, Math.PI * 2);
+    ctx.arc(cx, this.y + 15 + bob, 9, 0, Math.PI * 2);
+    ctx.fill();
+
+    // Eye glow
+    ctx.fillStyle = "#e8d5b7";
+    const eyeX = this.facing === 1 ? cx + 4 : cx - 4;
+    ctx.beginPath();
+    ctx.arc(eyeX, this.y + 15 + bob, 2.4, 0, Math.PI * 2);
+    ctx.fill();
+
+    // Soft glow around eye
+    ctx.fillStyle = "rgba(232, 213, 183, 0.25)";
+    ctx.beginPath();
+    ctx.arc(eyeX, this.y + 15 + bob, 4.5, 0, Math.PI * 2);
     ctx.fill();
   }
 }
